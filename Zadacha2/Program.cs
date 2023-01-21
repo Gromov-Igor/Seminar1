c5df937acf98e972851b6a2ec8ce42caf86e39ce
Gromov Igor <gaz.oil.gromov@gmail.com>
2023-01-22 04:16:50 +0500

    Zadacha2

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -3,3 +3,19 @@
 // a = 2 b = 10 -> нет 
 // a = 9; b = -3 -> нет 
 // a = -3 b = 9 -> да
+// if(a * a == b)
+
+Console.WriteLine("введите первое число");
+String number = Console.ReadLine();
+int a = Convert.ToInt32(number);
+
+Console.WriteLine("введите второе число");
+String number1 = Console.ReadLine();
+int b = Convert.ToInt32(number1);
+
+if(b*b == a)
+{
+    System.Console.WriteLine($"число {a} является квадратом {b}");
+}
+else 
+    System.Console.WriteLine($"число {a} не является квадратом {b}");
