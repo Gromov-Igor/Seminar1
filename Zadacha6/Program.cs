5869b459296feb60524c59faa777d543825b2758
Gromov Igor <gaz.oil.gromov@gmail.com>
2023-01-24 23:07:16 +0500

    Zadacha6

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает значение, а  на выходе показывает обратное значение.
+// 1 -> 1 
+// 2 -> 0.5
+// 0.25 -> 4
+
+Console.WriteLine("Введите значение целое или не целое число"); // Вывод приглашения в терминале
+double result = 1/Convert.ToDouble(Console.ReadLine);
+
+double number = Convert.ToDouble(Console.ReadLine);
+double result = 1/number;
